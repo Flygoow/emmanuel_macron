be56fa3fb64553887d7b53bc8266ef71104d5f06
Jérémy <30751347+Flygoow@users.noreply.github.com>
2018-06-20 18:15:13 +0200

    Eh Manu !

---
+++ index.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+bot.on('message', (message) => {
+    if (message.author.equals(bot.user)) return;
+
+        if(message.content.match (/Manu/i)) {
+        message.channel.sendMessage("Non non non, je suis pas ton copain, hein, non non non non. Tu es là, dans une cérémonie officielle. Tu te comportes comme il faut ! Donc, tu peux faire l'imbécile, mais aujourd'hui c'est la Marseillaise et le Chant des Partisans, tu m'appelles « Monsieur le Président » ou « Monsieur ». Compris ? C'est bien. Et tu fais les choses dans le bon ordre, si le jour om tu veux faire la révolution, t'apprends d'abord à avoir un diplôme, et à te nourrir toi-même. D'accord ? Et à ce moment-là t'iras donner des leçons.")
+    }
+});
+
+bot.login(process.env.BOT_TOKEN);
